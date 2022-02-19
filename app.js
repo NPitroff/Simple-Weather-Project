724ee3b6706d69afb9be25a7991ce40919621d35
NPitroff <nicolai.pitroff@gmail.com>
2022-02-18 16:51:14 -0800

    added https and pulled information from the JSON

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+// native method to call api with node
+const https = require("https");
 const app = express();
 
 app.get("/", function(req, res){
+  // const to hold the url
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Rialto&appid=6d0e27a1bde81971c768423d4c6f86f7&units=imperial"
+  https.get(url, function(response){
+    console.log(response);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const feelsLike = weatherData.main.feels_like;
+      console.log(feelsLike);
+    })
+  })
   res.send("Server is up and running");
 })
 
