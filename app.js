aa8edb9104dd6855494688a755ac8404caadc9c3
NPitroff <nicolai.pitroff@gmail.com>
2022-02-19 09:04:09 -0800

    Restructuring program to allow for user input

--- app.js
+++ app.js
@@ -3,33 +3,41 @@ const express = require("express");
 const https = require("https");
 const app = express();
 
+
+
 app.get("/", function(req, res){
-  // const to hold the url
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Rialto&appid=6d0e27a1bde81971c768423d4c6f86f7&units=imperial"
-  https.get(url, function(response){
-    console.log(response);
-
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      // variable to hold icon
-      const icon = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+".png"
-      // console.log(weatherDescription);
-      const feelsLike = weatherData.main.feels_like;
-      // console.log(feelsLike);
-      res.write("<h1>The Temperature in Rialto is "+ temp + " degrees Farenheit</h1>");
-      res.write("<h2>Today calls for "+weatherDescription+ "</h2>")
-      res.write("<img id=wicon src="+icon+" alt=Weather icon>");
-      res.end();
-    })
-
-
-  })
+  res.sendFile(__dirname + "/index.html");
 
 })
 
-
+// // var to hold the query city
+// const query = "Rialto";
+// // var to hold appid
+// const apiKey = "6d0e27a1bde81971c768423d4c6f86f7";
+// // var to hold units
+// const units = "imperial";
+// // const to hold the url
+// const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+units;
+// https.get(url, function(response){
+//   console.log(response);
+//
+//   response.on("data", function(data){
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const weatherDescription = weatherData.weather[0].description;
+//     // variable to hold icon
+//     const icon = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+".png"
+//     // console.log(weatherDescription);
+//     const feelsLike = weatherData.main.feels_like;
+//     // console.log(feelsLike);
+//     res.write("<h1>The Temperature in Rialto is "+ temp + " degrees Farenheit</h1>");
+//     res.write("<h2>Today calls for "+weatherDescription+ "</h2>")
+//     res.write("<img id=wicon src="+icon+" alt=Weather icon>");
+//     res.end();
+//   })
+//
+//
+// })
 
 
 
